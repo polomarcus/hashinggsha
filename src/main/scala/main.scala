f08c4b9c5d182193cc9c68c3bac06eab4fcb0461
Albless <alban.delagrange@gmail.com>
2016-10-10 16:54:39 +0200

    modification of the hashing process

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -3,17 +3,65 @@ import scala.language.postfixOps
 
 object main extends App {
 
-  def hashmeSha256(json: String) = {
+    val publisherId = "publisherId"
+    val bidRequestId = "bidRequestId"
+    val exchange = "exchange"
+    val lineId = "lineId"
+    val ip = "ip"
+    val city = "city"
+    val os = "os"
+    val connectionType = "connectionType"
+ /*
+  def hashmeSha256(json: String) = json match {
+    case `publisherId` => publisherId.sha256                                          // Generate hashes
+      println ("SHA256: " + publisherId.sha256.hex)                                   // Print each hex encoded hash
+      println("SHA256 Matches: " + (publisherId.sha256 hash = publisherId.sha256) )   // Compare the original value to each hashed value
+                                                                                      // and print the boolean result
 
-    // Generate hashes
-    json.sha256
+    case `bidRequestId` => bidRequestId.sha256
+      println ("SHA256: " + bidRequestId.sha256.hex)
+      println("SHA256 Matches: " + (bidRequestId.sha256 hash = bidRequestId.sha256) )
 
-    // Print each hex encoded hash
-    println("SHA256: " + json.sha256.hex)
+    case `exchange` => exchange.sha256
+      println ("SHA256: " + exchange.sha256.hex)
+      println("SHA256 Matches: " + (exchange.sha256 hash = exchange.sha256) )
 
-    // Compare the original value to each hashed value
-    // and print the boolean result
-    println("SHA256 Matches: " + (json.sha256 hash = json.sha256))
+    case `ip` => ip.sha256
+      println ("SHA256: " + ip.sha256.hex)
+      println("SHA256 Matches: " + (ip.sha256 hash = ip.sha256) )
 
+    case `city` => city.sha256
+      println ("SHA256: " + city.sha256.hex)
+      println("SHA256 Matches: " + (city.sha256 hash = city.sha256) )
+
+    case `os` => os.sha256
+      println ("SHA256: " + os.sha256.hex)
+      println("SHA256 Matches: " + (os.sha256 hash = os.sha256) )
+
+    case `connectionType` => connectionType.sha256
+      println ("SHA256: " + connectionType.sha256.hex)
+      println("SHA256 Matches: " + (connectionType.sha256 hash = connectionType.sha256) )
+
+    case `lineId` => lineId.sha256
+      println ("SHA256: " + lineId.sha256.hex)
+      println("SHA256 Matches: " + (lineId.sha256 hash = lineId.sha256) )
   }
+
+
+*/
+
+def hashMeSha256(json: String) = {
+
+  // Generate a few hashes
+  val sha256 = json.sha256
+
+
+  // Print each hex encoded hash
+  println( "SHA1: " + sha256.hex )
+  
+}
+
+
+
+
 }
--- src/test/scala/hasingTest.scala
+++ src/test/scala/hasingTest.scala
@@ -16,7 +16,7 @@ class hashingTest extends WordSpec {
 
       val hashJson =
         """
-          |"appOrSite":"app","bidfloor":0.28,"cats":"IAB24,IAB9,IAB9-23,photography,social_networking",
+          |{"appOrSite":"app","bidfloor":0.28,"cats":"IAB24,IAB9,IAB9-23,photography,social_networking",
           |"city":"Saint-laurent-blangy","connectionType":2,"exchange":"mopub", "id":"dd8048ce-776c-49c0-b187-efbaea028115","ip":"78.200.141.1",
           |"lineId":"9044e701-a476-47b3-a6c2-434482212e34","media":"banner","os":"iOS", "price":3.71,"publisherId":"95df5d30a25311e295fa123138070049", "size":[320,480],
           |"timestamp":1.466380953E9,"ua":"Mozilla/5.0 (iPod touch; CPU iPhone OS 9_2_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Mobile/13D15",
@@ -25,7 +25,7 @@ class hashingTest extends WordSpec {
         """.
           stripMargin
 
-      main.hashmeSha256(hashJson)
+      main.hashMeSha256(hashJson)
 
     }
   }
